6c9cac12b4ccfa191ca229c6ccef29b6a719b2dd
Andras Toth <andras.toth93@gmail.com>
2015-03-18 03:03:43 +0100

    Using LocalStorage events

--- static/main.js
+++ static/main.js
@@ -1,33 +1,63 @@
 (function() {
   var app = angular.module('ws', []);
 
-  app.controller('MainController', function($scope, WebSocketService) {
+  app.controller('MainController', function($scope, WebSocketService, Tick) {
     self = this;
-    this.tick = 0;
-
-    $scope.$on(WebSocketService.tickEvent, function() {
-      $scope.$apply(function(){
-        self.tick = WebSocketService.getTick();
-      });
-    });
+    this.tick = WebSocketService.getTick();
   });
 
-  app.service('WebSocketService', function($location, $rootScope) {
-    var self = this;
-    var tick = 0;
-    var host = $location.host();
-    var port = $location.port();
-
-    var ws = new WebSocket('ws://' + host + ':' + port + '/tick');
-
-    this.tickEvent = 'tickEvent';
-    ws.onmessage = function(e) {
-      tick = e.data;
-      $rootScope.$broadcast(self.tickEvent);
-    }
+  app.service('WebSocketService', function($window, $location, $rootScope, Tick) {
+    var key = 'i';
+    var workerKey = 'w';
+    var closingKey = 'c';
+    var worker = (localStorage[workerKey] === 'true');
 
     this.getTick = function() {
-      return tick;
+      return Tick;
     }
+
+    var openWs = function() {
+      var host = $location.host();
+      var port = $location.port();
+      var ws = new WebSocket('ws://' + host + ':' + port + '/tick');
+      localStorage[workerKey] = true;
+
+      ws.onmessage = function(e) {
+        $rootScope.$apply(function() {
+          Tick.i = e.data;
+          localStorage[key] = e.data;
+        });
+      }
+
+      ws.onclose = function(e) {
+        localStorage[closingKey] = true;
+      }
+
+      angular.element($window).on('unload', function(e) {
+        localStorage.removeItem(workerKey);
+        ws.close();
+      });
+    }
+
+    if (!worker) {
+      openWs();
+    } else {
+      angular.element($window).on('storage', function(e) {
+        if (e.key === key) {
+          $rootScope.$apply(function() {
+            Tick.i = e.newValue;
+          });
+        } else if (e.key === closingKey) {
+          openWs();
+        }
+      });
+    }
+  });
+
+  app.factory('Tick', function() {
+    var i = 0;
+    return {
+      i: i
+    };
   });
 })();
